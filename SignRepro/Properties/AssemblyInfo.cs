85eda6b08480f78001879c2609e9744562fca4ac
JohanLarsson <johan_gavle1@hotmail.com>
2018-07-18 12:14:34 +0200

    Initial

---
+++ SignRepro/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SignRepro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Johan Larsson")]
+[assembly: AssemblyProduct("SignRepro")]
+[assembly: AssemblyCopyright("Copyright © Johan Larsson 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("6f505b8e-df46-421a-8738-1a655613d760")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
